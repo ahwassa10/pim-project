b6f2f7e6efdb0c039afb4e3709ee190559cfa777
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-02-25 18:40:22 -0500

    Integrated MemEntity into the IMS.
    This also required changing the internal representation of the IdentityTable.

--- ims/src/inmemory/IMS.java
+++ ims/src/inmemory/IMS.java
@@ -5,6 +5,7 @@ import model.properties.Property;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import model.entities.Entity;
 import model.entities.Link;
@@ -13,16 +14,21 @@ public class IMS {
 	private IdentityTable identityTable = new IdentityTable();
 	
 	public Entity createEntity() {
+		Identity i = new Identity() {
+			private UUID uuid;
+			public UUID getIdentity() {
+				return this.uuid;
+			}
+		};
+		
 		Entity e = new Entity() {
 			public List<Property> getProperties() {
-				return null;
+				return List.of(i);
 			}
 			public Set<Link> getLinks() {
 				return null;
 			}
 		};
-		
-		Identity i = new MemIdentity();
 		
 		identityTable.set(i, e);
 		return e;
--- ims/src/inmemory/IdentityTable.java
+++ ims/src/inmemory/IdentityTable.java
@@ -2,26 +2,27 @@ package inmemory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import model.entities.Entity;
 import model.properties.Identity;
 
 public class IdentityTable {
-	private Map<Identity, Entity> map = new HashMap<>();
+	private Map<UUID, Entity> map = new HashMap<>();
 	
 	public boolean contains(Identity identity) {
-		return map.containsKey(identity);
+		return map.containsKey(identity.getIdentity());
 	}
 	
 	public void set(Identity identity, Entity entity) {
-		map.put(identity, entity);
+		map.put(identity.getIdentity(), entity);
 	}
 	
 	public Entity read(Identity identity) {
-		return map.get(identity);
+		return map.get(identity.getIdentity());
 	}
 	
 	public void delete(Identity identity) {
-		map.remove(identity);
+		map.remove(identity.getIdentity());
 	}
 }
