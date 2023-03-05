1a1083187be7d614c44b59c341b4a18d7673f6f4
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-03-05 15:01:48 -0500

    Created an experimental project to test Java generics

---
+++ experimental/src/generics/Test1.java
@@ -0,0 +1,38 @@
+package generics;
+
+import java.util.List;
+
+public class Test1 {
+	// T is a type parameter
+	public static class Capsule<T> {
+		private T t;
+		public void set(T t) {
+			this.t = t;
+		}
+		public T get() {
+			return this.t;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// Integer is a type argument
+		// "Capsule<Integer> c" is a generic type invocation. The type argument
+		// Integer is passed to the Capsule class itself. The invocation of a 
+		// generic type is known as a parameterized type.
+		Capsule<Integer> c = new Capsule<>();
+		c.set(25);
+		System.out.println(c.get());
+		
+		Capsule<List<Integer>> c2 = new Capsule<>();
+		c2.set(List.of(1, 2, 3));
+		System.out.println(c2.get().toString());
+		
+		// Capsule is the raw type of the genertic type Capsule<T>. 
+		// Raw types seem to be a vestige of Java code before generics.
+		// Capsule c3 = new Capsule();
+		// c3.set("test");
+		//System.out.println(c3.get());
+		
+	}
+
+}
