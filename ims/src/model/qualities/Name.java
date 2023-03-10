81fb9db37743e8d9e5f90aa297929dbe6c93671b
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-03-09 21:21:36 -0500

    Implemented Name as a quality

--- ims/src/model/qualities/Name.java
+++ ims/src/model/qualities/Name.java
@@ -1,14 +1,46 @@
 package model.qualities;
 
-
-import java.util.List;
-
-public class Name implements InformationType {
-	public QualityType getName() {
-		return QualityType.from("name");
+public class Name implements Quality {
+	
+	private static final int MAX_NAME_LENGTH = 128;
+	
+	private static QualityType qualityType =
+			QualityType.from("name");
+	
+	private String nameValue;
+	
+	private Name(String nameValue) {
+		if (!isValidNameValue(nameValue)) {
+			throw new IllegalArgumentException("Invalid name value");
+		} else {
+			this.nameValue = nameValue;
+		}
 	}
 	
-	public List<Piece<?>> getProperties() {
-		return null;
+	public static boolean isValidNameValue(String test_string) {
+		if ((test_string == null) ||
+			(test_string.length() <= 0) ||
+			(test_string.length() > MAX_NAME_LENGTH)) {
+			return false;
+		}
+		for (char c : test_string.toCharArray()) {
+			// Checks if c is a printable ascii character.
+			if (c < 32 || c >= 127) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static Name from(String nameValue) {
+		return new Name(nameValue);
+	}
+	
+	public QualityType getQualityType() {
+		return qualityType;
+	}
+	
+	public String toString() {
+		return nameValue;
 	}
 }
