b21d6995c292c9712d2807e22f985e257850f626
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-03-09 21:30:59 -0500

    Implemented Tag as a quality
    The problem of multiplicity still remains.

--- ims/src/model/qualities/Tag.java
+++ ims/src/model/qualities/Tag.java
@@ -1,11 +1,52 @@
 package model.qualities;
 
-import model.real.Keyword;
+import java.util.Arrays;
 
-public interface Tag extends InformationType {
-	public default QualityType getName() {
-		return QualityType.from("tag");
+public class Tag implements Quality {
+	
+	private static final char[] ALLOWED_KEYWORD_CHARACTERS =
+			new char[] {'_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+				        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+				        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	
+	private static final int MAX_KEYWORD_LENGTH = 32;
+	
+	private static QualityType qualityType = 
+			QualityType.from("tag");
+	
+	private String keyword;
+	
+	private Tag(String keyword) {
+		if (!isValidKeyword(keyword)) {
+			throw new IllegalArgumentException("Invalid tag name");
+		} else {
+			this.keyword = keyword;
+		}
 	}
 	
-	public Keyword getValue();
+	public static boolean isValidKeyword(String test_string) {
+		if ((test_string == null) ||
+				(test_string.length() <= 0) ||
+				(test_string.length() > MAX_KEYWORD_LENGTH)) {
+				return false;
+			}
+		for (char c : test_string.toCharArray()) {
+			if (Arrays.binarySearch(ALLOWED_KEYWORD_CHARACTERS, c) < 0) {
+				return false;
+				}
+		}
+		return true;
+	}
+	
+	public static Tag from(String keyword) {
+		return new Tag(keyword);
+	}
+	
+	public QualityType getQualityType() {
+		return qualityType;
+	}
+	
+	public String toString() {
+		return keyword;
+	}
 }
