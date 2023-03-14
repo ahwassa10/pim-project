a20cf67903693c35b8d7e741e30681c938f20388
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-03-14 13:01:27 -0400

    Converted Pair and Triple into Interfaces

--- ims/src/structural/Pair.java
+++ ims/src/structural/Pair.java
@@ -1,43 +1,6 @@
 package structural;
 
-public final class Pair<K, V> {
-	private final K key;
-	private final V value;
-	
-	private Pair(K key, V value) {
-		this.key = key;
-		this.value = value;
-	}
-	
-	public static <K, V> Pair<K, V> of(K key, V value) {
-		return new Pair<K, V>(key, value);
-	}
-	
-	public boolean equals(Object o) {
-		if (o == this) {return true;}
-		if (!(o instanceof Pair<?, ?>)) {return false;}
-		
-		Pair<?, ?> p = (Pair<?, ?>) o;
-		return key.equals(p.key) && value.equals(p.value);
-	}
-	
-	public K getKey() {
-		return key;
-	}
-	
-	public V getValue() {
-		return value;
-	}
-	
-	public int hashCode() {
-		// Hashcode recipe provided in Effective Java 3ed.
-		int result = key.hashCode();
-		result = 31 * result + value.hashCode();
-		
-		return result;
-	}
-	
-	public String toString() {
-		return String.format("Pair<%s, %s>", key, value);
-	}
+public interface Pair<K, V> {
+	public K getKey();
+	public V getValue();
 }
--- ims/src/structural/Triple.java
+++ ims/src/structural/Triple.java
@@ -1,58 +1,7 @@
 package structural;
 
-public class Triple<K, Q, V> {
-	private final K key;
-	private final Q qualifier;
-	private final V value;
-	
-	private Triple(K key, Q qualifier, V value) {
-		this.key = key;
-		this.qualifier = qualifier;
-		this.value = value;
-	}
-	
-	public static <K, Q, V> Triple<K, Q, V> of (K key, Q qualifier, V value) {
-		return new Triple<K, Q, V>(key, qualifier, value);
-	}
-	
-	public Pair<K, Pair<Q, V>> asPairs() {
-		return Pair.of(key, Pair.of(qualifier, value));
-	}
-	
-	public boolean equals(Object o) {
-		if (o == this) {return true;}
-		if (!(o instanceof Triple<?, ?, ?>)) {return false;}
-		
-		Triple<?, ?, ?> t = (Triple<?, ?, ?>) o;
-		
-		return key.equals(t.key) &&
-			   qualifier.equals(t.qualifier) &&
-			   value.equals(t.value);
-	}
-	
-	public K getKey() {
-		return key;
-	}
-	
-	public V getValue() {
-		return value;
-	}
-	
-	public Q getQualifier() {
-		return qualifier;
-	}
-	
-	public int hashCode() {
-		// Hashcode recipe provided in Effective Java 3ed.
-		int result = key.hashCode();
-		result = 31 * result + qualifier.hashCode();
-		result = 31 * result + value.hashCode();
-		
-		return result;
-	}
-	
-	public String toString() {
-		return String.format("Triple<%s, %s, %s>",
-				key, qualifier, value);
-	}
+public interface Triple<K, Q, V> {
+	public K getKey();
+	public Q getQualifier();
+	public V getValue();
 }
