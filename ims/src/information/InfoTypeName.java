bbf519eca1cef7fa7e23a1e5ed7d08e6d4e95432
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-03-15 21:37:59 -0400

    Changed the requirements for a valid infoTypeName

--- ims/src/information/InfoTypeName.java
+++ ims/src/information/InfoTypeName.java
@@ -1,17 +1,30 @@
 package information;
 
-import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class InfoTypeName {
-	
-	private static final char[] ALLOWED_CHARACTERS =
-			new char[] {'-', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-					    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-					    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	
+	// Matches a string that starts with an uppercase letter followed by
+	// zero or more occurrences of any letter (both uppercase and
+	// lowercase) or the hyphen -.
+	private static Pattern pattern = Pattern.compile("[A-Z][a-zA-Z[-]]*");
 	private static final int MAX_LENGTH = 128;
+	private static final int MIN_LENGTH = 0;
 	
 	private String infoTypeName;
+	
+	public static InfoTypeName from(String infoTypeName) {
+		return new InfoTypeName(infoTypeName);
+	}
+	
+	public static boolean isValidQualityType(String test_string) {
+		if ((test_string == null) ||
+			(test_string.length() < MIN_LENGTH) ||
+			(test_string.length() > MAX_LENGTH)) {
+			return false;
+		}
+		
+		return pattern.matcher(test_string).matches();
+	}
 	
 	private InfoTypeName(String in) {
 		if (!isValidQualityType(in)) {
@@ -31,24 +44,6 @@ public class InfoTypeName {
 	
 	public int hashCode() {
 		return infoTypeName.hashCode();
-	}
-	
-	public static boolean isValidQualityType(String test_string) {
-		if ((test_string == null) ||
-			(test_string.length() <= 0) ||
-			(test_string.length() > MAX_LENGTH)) {
-			return false;
-		}
-		for (char c : test_string.toCharArray()) {
-			if (Arrays.binarySearch(ALLOWED_CHARACTERS, c) < 0) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	public static InfoTypeName from(String infoTypeName) {
-		return new InfoTypeName(infoTypeName);
 	}
 	
 	public String toString() {
