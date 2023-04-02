5877fa3f8f8a0789301be5d25f355064ad5a6f8c
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-04-01 20:28:43 -0400

    Reorganized methods in SubstanceStore and Hashing

--- ims/src/substance/SubstanceStore.java
+++ ims/src/substance/SubstanceStore.java
@@ -1,13 +1,23 @@
 package substance;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.EnumSet;
 
 import util.Hashing;
 
+
 public final class SubstanceStore {
+	private static final EnumSet<StandardOpenOption> WRITE_OPTIONS =
+			EnumSet.of(StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
+	
 	private final Path substance_folder;
 	
 	SubstanceStore(Path substance_folder) {
@@ -16,12 +26,42 @@ public final class SubstanceStore {
 		System.out.println("Successfully created a substance store");
 	}
 	
-	public String capture(Path substanceSource) {
-		Path tempFile = substance_folder.resolve("temp");		
+	private static byte[] hashAndCopy(Path src, Path dest) throws IOException {
+		MessageDigest digest = null;
 		try {
-			String hash = Hashing.hashStringAndCopy(substanceSource, tempFile);
-			Path substanceFile = substance_folder.resolve(hash);
+			digest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		ByteBuffer buffer = ByteBuffer.allocate(8192);
+		try (ByteChannel srcChannel = Files.newByteChannel(src);
+			 ByteChannel destChannel = Files.newByteChannel(dest, WRITE_OPTIONS)) {
 			
+			// Calculate hash and copy to destination buffer without having to read
+			// the input file twice. 
+			while (srcChannel.read(buffer) != -1) {
+				buffer.flip();
+				digest.update(buffer);
+				buffer.flip();
+				destChannel.write(buffer);
+				buffer.clear();
+			}
+		}
+		return digest.digest();
+	}
+	
+	public String capture(Path substanceSource) {
+		if (substanceSource == null) {
+			throw new IllegalArgumentException("Substance source cannot be null");
+		}
+		
+		Path tempFile = substance_folder.resolve("temp");
+		try {
+			String hash = Hashing.asString(hashAndCopy(substanceSource, tempFile));
+			
+			Path substanceFile = substance_folder.resolve(hash);
 			if (Files.exists(substanceFile)) {
 				// The case where the substance is a duplicate
 				Files.delete(tempFile);
--- ims/src/util/Hashing.java
+++ ims/src/util/Hashing.java
@@ -5,32 +5,27 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.EnumSet;
 import java.util.HexFormat;
 
 public final class Hashing {
 	private static final HexFormat HEX_FORMATTER =
 			HexFormat.of().withUpperCase();
 	
-	private static final EnumSet<StandardOpenOption> DEST_OPTIONS =
-			EnumSet.of(StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
-	
 	private Hashing() {}
 	
-	public static byte[] calculateHash(Path file) throws IOException {
-		if (file == null) {
+	public static String asString(byte[] hash) {
+		return HEX_FORMATTER.formatHex(hash);
+	}
+	
+	public static byte[] calculateHash(Path file, String algorithm)
+	throws IOException, NoSuchAlgorithmException {
+		if (file == null || algorithm == null) {
 			throw new IllegalArgumentException("Inputs cannot be null");
 		}
 		
-		MessageDigest digest = null;
-		try {
-			digest = MessageDigest.getInstance("SHA-256");
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
+		MessageDigest digest = MessageDigest.getInstance(algorithm);
 		
 		try (ByteChannel byteChannel = Files.newByteChannel(file)) {
 			ByteBuffer buffer = ByteBuffer.allocate(8192);
@@ -43,41 +38,15 @@ public final class Hashing {
 		return digest.digest();
 	}
 	
-	public static String calculateHashString(Path file) throws IOException {
-		byte[] hash = calculateHash(file);
-		return HEX_FORMATTER.formatHex(hash);
-	}
-	
-	public static byte[] hashAndCopy(Path sourceFile, Path destFile) throws IOException {
-		if (sourceFile == null || destFile == null) {
-			throw new IllegalArgumentException("Inputs cannot be null");
+	public static byte[] calculateSHA256(Path file) throws IOException {
+		if (file == null) {
+			throw new IllegalArgumentException("File cannot be null");
 		}
-		
-		MessageDigest digest = null;
 		try {
-			digest = MessageDigest.getInstance("SHA-256");
+			return calculateHash(file, "SHA-256");
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
+			return null;
 		}
-		
-		ByteBuffer buffer = ByteBuffer.allocate(8192);
-		try (ByteChannel sourceChannel = Files.newByteChannel(sourceFile);
-			 ByteChannel destChannel = Files.newByteChannel(destFile, DEST_OPTIONS)) {
-			
-			while (sourceChannel.read(buffer) != -1) {
-				buffer.flip();
-				digest.update(buffer);
-				buffer.flip();
-				destChannel.write(buffer);
-				buffer.clear();
-			}
-		}
-		
-		return digest.digest();
-	}
-	
-	public static String hashStringAndCopy(Path sourceFile, Path destFile) throws IOException {
-		byte[] hash = hashAndCopy(sourceFile, destFile);
-		return HEX_FORMATTER.formatHex(hash);
 	}
 }
