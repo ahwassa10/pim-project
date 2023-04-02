13c50469a3420d0417c07ebf3cce67cc2f57b5ae
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-04-01 20:36:14 -0400

    Fixed Testing2

--- ims/src/program/Testing2.java
+++ ims/src/program/Testing2.java
@@ -8,10 +8,10 @@ public class Testing2 {
 	public static void main(String[] args) {
 		Path folder = Path.of("C:\\Users\\Primary\\Desktop\\import");
 		try {
-			System.out.println(Hashing.calculateHashString(folder.resolve("3080ti.JPG")));
-			System.out.println(Hashing.calculateHashString(folder.resolve("EndTimes.JPG")));
-			System.out.println(Hashing.calculateHashString(folder.resolve("leetcodepfp.png")));
-			System.out.println(Hashing.calculateHashString(folder.resolve("empty.txt")));
+			System.out.println(Hashing.asString(Hashing.calculateSHA256(folder.resolve("3080ti.JPG"))));
+			System.out.println(Hashing.asString(Hashing.calculateSHA256(folder.resolve("EndTimes.JPG"))));
+			System.out.println(Hashing.asString(Hashing.calculateSHA256(folder.resolve("leetcodepfp.png"))));
+			System.out.println(Hashing.asString(Hashing.calculateSHA256(folder.resolve("empty.txt"))));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
