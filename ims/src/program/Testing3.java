a454cb2c7165b4f872fb8a30baaefaee6560c4fb
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-04-08 11:44:09 -0400

    Modified the key_pattern

--- ims/src/program/Testing3.java
+++ ims/src/program/Testing3.java
@@ -8,7 +8,8 @@ public class Testing3 {
     public static void main(String[] args) {
         List<String> testKeys = List.of("hello", "123", "he11o", "h#llo", "#", "", "1",
                 "hello_there", "there", "there123", "hello there", "__hello--", "$hello",
-                "$", ":bricks:", " ", "t t t -", "   ", "abc_123-def");
+                "$", ":bricks:", " ", "t t t -", "   ", "abc_123-def", "user.name",
+                "user.name.1234", "filesystem.file_size.entity-2431", "....", "user$name");
         
         for (String test : testKeys) {
             System.out.println(Key.isValid(test) + " " + test);
--- ims/src/quality/Key.java
+++ ims/src/quality/Key.java
@@ -3,7 +3,7 @@ package quality;
 import java.util.regex.Pattern;
 
 public interface Key {
-    Pattern KEY_PATTERN = Pattern.compile("^[a-zA-Z0-9_\\-]{1,255}$");
+    Pattern KEY_PATTERN = Pattern.compile("^[a-z0-9_.\\-]{1,255}$");
     
     static boolean isValid(String test_string) {
         return KEY_PATTERN.matcher(test_string).matches();
