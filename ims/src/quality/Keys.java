cac9bc11e66df6141c3b7307ede6a718855d55d3
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-04-08 22:22:11 -0400

    QualityStore improvements

--- ims/src/quality/Keys.java
+++ ims/src/quality/Keys.java
@@ -5,15 +5,30 @@ import java.util.regex.Pattern;
 
 public final class Keys {
     private static final Pattern KEY_PATTERN = Pattern.compile("^[a-z0-9_.\\-]{1,255}$");
+    private static final String KEY_SEPARATOR = "$";
+    private static final Pattern KEY_SPLIT_PATTERN = Pattern.compile("\\$");
     
     private Keys() {}
     
+    public static String getKeySeparator() {
+        return KEY_SEPARATOR;
+    }
+    
+    public static String combine(String key1, String key2) {
+        return key1 + KEY_SEPARATOR + key2;
+    }
+    
+    public static String combine(String key1, String key2, String key3) {
+        return key1 + KEY_SEPARATOR + key2 + KEY_SEPARATOR + key3;
+    }
+    
     public static boolean isValid(String test_string) {
-        return KEY_PATTERN.matcher(test_string).matches();
+        return test_string != null &&
+               KEY_PATTERN.matcher(test_string).matches();
     }
     
     public static String requireValidKey(String test_string) {
-        Objects.requireNonNull(test_string, "Key cannot be null");
+        Objects.requireNonNull(test_string, "Input cannot be null");
         
         if (Keys.isValid(test_string)) {
             return test_string;
@@ -21,5 +36,9 @@ public final class Keys {
             String msg = String.format("%s is not a valid key", test_string);
             throw new IllegalArgumentException(msg);
         }
+    }
+    
+    public static String[] split(String multiKey) {
+        return KEY_SPLIT_PATTERN.split(multiKey);
     }
 }
--- ims/src/quality/QualityStore.java
+++ ims/src/quality/QualityStore.java
@@ -1,6 +1,7 @@
 package quality;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
@@ -11,17 +12,17 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 public final class QualityStore {
-    private final Map<String, Map<String, Set<String>>> events;
+    private final Map<String, Map<String, Set<String>>> keyMap;
     private final Path export_folder;
     private final Path quality_folder;
 
     QualityStore(Path ef_path, Path qf_path) {
-        this.events = new HashMap<>();
+        this.keyMap = new HashMap<>();
         this.export_folder = ef_path;
         this.quality_folder = qf_path;
 
         try {
-            loadIndex();
+            load();
             System.out.println("Successfully created a quality store");
         } catch (IOException e) {
             e.printStackTrace();
@@ -29,134 +30,146 @@ public final class QualityStore {
         }
     }
     
-    public boolean delete(String agent,
-                          String type,
-                          String entity) throws IOException {
+    public String get(String primaryKey,
+                      String secondaryKey,
+                      String tertiaryKey) throws IOException {
+
+        Keys.requireValidKey(primaryKey);
+        Keys.requireValidKey(secondaryKey);
+        Keys.requireValidKey(tertiaryKey);
         
-        Keys.requireValidKey(agent);
-        Keys.requireValidKey(type);
-        Keys.requireValidKey(entity);
-        
-        Path eventPath = quality_folder.resolve(agent).resolve(type).resolve(entity);
-       
-        boolean foundOnDisk = Files.deleteIfExists(eventPath);
-        boolean foundInIndex = events.containsKey(agent) && // Checks if agent exists
-                               events.get(agent).containsKey(type) && // if qualityType exists
-                               events.get(agent).get(type).remove(entity); // if event exists
-                               // Note that remove() returns boolean if the entity was removed.
-        
-        
-        if (foundOnDisk != foundInIndex) {
-            // Issues a warning when an inconsistency is found between the the disk and 
-            // the index. The system recovers from this inconsistency by trying to 
-            // delete both the file on disk, and the mapping in the index. 
-            System.out.println(String.format("Warning: Event<%s, %s, %s> was found %s",
-                    agent, type, entity,
-                    foundOnDisk ? "on-disk but not in-index" : "in-index but not on-disk"));
+        if (!keyMap.containsKey(primaryKey)) {
+          String message = String.format("Primary key: %s does not exist", primaryKey);
+          throw new IllegalArgumentException(message);
+        }
+        if (!keyMap.get(primaryKey).containsKey(secondaryKey)) {
+          String message = String.format("Seconday key: %s does not exist", secondaryKey);
+          throw new IllegalArgumentException(message);
+        }
+        if (!keyMap.get(primaryKey).get(secondaryKey).contains(tertiaryKey)) {
+          String message = String.format("Tertiary key: %s does not exist", tertiaryKey);
+          throw new IllegalArgumentException(message);
         }
         
-        // Returns true as long as something was deleted from either the on-disk or in-index.
-        return foundOnDisk || foundInIndex;
+        String fullKey = Keys.combine(primaryKey, secondaryKey, tertiaryKey);
+        Path fullKeyPath = quality_folder.resolve(fullKey);
+        return Files.readString(fullKeyPath);
     }
     
-    public void deleteAll() throws IOException {
-        events.clear();
-        try (Stream<Path> walk = Files.walk(quality_folder, 3)) {
-            walk.filter(path -> path.compareTo(quality_folder) != 0)
-                .sorted(Comparator.reverseOrder())
-                .forEach(path -> {
-                    try {
-                        Files.delete(path);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+    private void load() throws IOException {
+        try (DirectoryStream<Path> qualityStream = Files.newDirectoryStream(quality_folder)) {
+            for (Path qualityPath : qualityStream) {
+                if (Files.isRegularFile(qualityPath)) {
+                    String fullKey = qualityPath.getFileName().toString();
+                    String[] keys = Keys.split(fullKey);
+                    
+                    if (keys.length != 3) {
+                        String message = String.format("%s is not a valid full key", fullKey);
+                        System.out.println(message);
+                        continue;
                     }
-                });
-        }
-    }
-    
-    private void loadIndex() throws IOException {
-        try (Stream<Path> walk = Files.walk(quality_folder, 3)) {
-            walk.forEach(path -> {
-                path = quality_folder.relativize(path);
-                int count = path.getNameCount();
-                
-                if (count == 1 && !path.endsWith("")) {
-                    // Case where path is an agent
-                    String agent = path.toString();
-                    events.put(agent, new HashMap<>());
-                } else if (count == 2) {
-                    // Case where path is a type
-                    String agent = path.subpath(0, 1).toString();
-                    String type = path.subpath(1, 2).toString();
-                    events.get(agent).put(type, new HashSet<>());
-                } else if (count == 3) {
-                    // Case where path is an entity
-                    String agent = path.subpath(0, 1).toString();
-                    String type = path.subpath(1, 2).toString();
-                    String entity = path.subpath(2, 3).toString();
-                    events.get(agent).get(type).add(entity);
+                    
+                    String primaryKey = keys[0];
+                    String secondaryKey = keys[1];
+                    String tertiaryKey = keys[2];
+                    
+                    if (!Keys.isValid(primaryKey)) {
+                        String message = String.format("%s is not a valid primary key in %s",
+                                primaryKey, fullKey);
+                        System.out.println(message);
+                        continue;
+                    } else if (!Keys.isValid(secondaryKey)) {
+                        String message = String.format("%s is not a valid secondary key in %s",
+                                secondaryKey, fullKey);
+                        System.out.println(message);
+                        continue;
+                    } else if (!Keys.isValid(tertiaryKey)) {
+                        String message = String.format("%s is not a valid tertiary key in %s",
+                                tertiaryKey, fullKey);
+                        System.out.println(message);
+                        continue;
+                    }
+                    
+                    // Add the full key to the keyMap
+                    keyMap.computeIfAbsent(primaryKey, k -> new HashMap<>())
+                          .computeIfAbsent(secondaryKey, k -> new HashSet<>())
+                          .add(tertiaryKey);
+                    
+                } else {
+                    String message = String.format("%s is not a quality", qualityPath);
+                    System.out.println(message);
                 }
-            });
+            }
         }
     }
     
     public void printIndex() {
-        System.out.println(events);
+        System.out.println(keyMap);
     }
     
-    public String retrieve(String agent,
-                           String type,
-                           String entity) throws IOException {
+    public void put(String primaryKey,
+                    String secondaryKey,
+                    String tertiaryKey,
+                    String value) throws IOException {
 
-        Keys.requireValidKey(agent);
-        Keys.requireValidKey(type);
-        Keys.requireValidKey(entity);
+        Keys.requireValidKey(primaryKey);
+        Keys.requireValidKey(secondaryKey);
+        Keys.requireValidKey(tertiaryKey);
         
-        if (!events.containsKey(agent)) {
-            throw new IllegalArgumentException("This agent does not exist");
-        }
-        if (!events.get(agent).containsKey(type)) {
-            throw new IllegalArgumentException("This qualityType does not exist");
-        }
-        if (!events.get(agent).get(type).contains(entity)) {
-            throw new IllegalArgumentException("This event does not exist");
-        }
+        // Write the quality to disk
+        String fullKey = Keys.combine(primaryKey, secondaryKey, tertiaryKey);
+        Path fullKeyPath = quality_folder.resolve(fullKey);
+        Files.writeString(fullKeyPath, value);
         
-        Path dataPath = quality_folder.resolve(agent).resolve(type).resolve(entity);
-        return Files.readString(dataPath);
+        // Add the full key to the keyMap
+        keyMap.computeIfAbsent(primaryKey, k -> new HashMap<>())
+              .computeIfAbsent(secondaryKey, k -> new HashSet<>())
+              .add(tertiaryKey);
     }
     
-    public void store(String agent,
-                      String type,
-                      String entity,
-                      String data) throws IOException {
+    public boolean remove(String primaryKey,
+                          String secondaryKey,
+                          String tertiaryKey) throws IOException {
 
-        Keys.requireValidKey(agent);
-        Keys.requireValidKey(type);
-        Keys.requireValidKey(entity);
-
-        // Creates the Agent and Type folders if they don't exist.
-        // Does nothing if the folders do exist.
-        Path qualityTypePath = quality_folder.resolve(agent).resolve(type);
-        Files.createDirectories(qualityTypePath);
-
-        // The UUID of the entity becomes the fileName.
-        Path eventPath = qualityTypePath.resolve(entity);
-
-        // Creates the file (if it doesn't exist) and writes the string
-        // to the file, truncating the file if it already has data in it.
-        Files.writeString(eventPath, data);
+        Keys.requireValidKey(primaryKey);
+        Keys.requireValidKey(secondaryKey);
+        Keys.requireValidKey(tertiaryKey);
         
-        // Index the agent if it doesn't exist.
-        if (!events.containsKey(agent)) {
-            events.put(agent, new HashMap<>());
+        String fullKey = Keys.combine(primaryKey, secondaryKey, tertiaryKey);
+        Path keyPath = quality_folder.resolve(fullKey);
+        
+        boolean foundOnDisk = Files.deleteIfExists(keyPath);
+        boolean foundInMap = keyMap.containsKey(primaryKey) &&
+                       keyMap.get(primaryKey).containsKey(secondaryKey) &&
+                       keyMap.get(primaryKey).get(secondaryKey).remove(tertiaryKey);
+        
+        
+        if (foundOnDisk != foundInMap) {
+        // Issues a warning when an inconsistency is found between the the disk and 
+        // the map. The system recovers from this inconsistency by trying to 
+        // delete both the file on disk, and the mapping in the index. 
+        System.out.println(String.format("Warning: FullKey<%s, %s, %s> was found %s",
+              primaryKey, secondaryKey, tertiaryKey,
+              foundOnDisk ? "on-disk but not in-map" : "in-map but not on-disk"));
         }
-        // Index the type if it doesn't exist.
-        if (!events.get(agent).containsKey(type)) {
-            events.get(agent).put(type, new HashSet<>());
+        
+        // Returns true as long as something was deleted from either the on-disk or in-index.
+        return foundOnDisk || foundInMap;
+    }
+
+    public void removeAll() throws IOException {
+        keyMap.clear();
+        
+        try (DirectoryStream<Path> QualityStream = Files.newDirectoryStream(quality_folder)) {
+            for (Path qualityPath : QualityStream) {
+                if (Files.isRegularFile(qualityPath)) {
+                    Files.delete(qualityPath);
+                } else {
+                    String message = String.format("%s is not a quality", qualityPath);
+                    System.out.println(message);
+                }
+            }
         }
-        // Create the agent+type -> entity association.
-        events.get(agent).get(type).add(entity);
     }
 
     public String toString() {
