1024867609108c0e0dd638f33002acdf20ce9942
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-04-19 13:20:39 -0400

    Finished unifying selection tags and regular tags

--- ims/src/qualitygardens/taggarden/TagGarden.java
+++ ims/src/qualitygardens/taggarden/TagGarden.java
@@ -43,6 +43,13 @@ public final class TagGarden {
             throw new IllegalArgumentException(msg);
         }
         
+        String selectionTagValue = getSelectionTagValue(tag_name, key);
+        if (selectionTagValue != null) {
+            String msg = String.format("%s is already associated with %s",
+                    key, Keys.combine(fullSelectionTagKey, selectionTagValue));
+            throw new IllegalArgumentException(msg);
+        }
+        
         String fullSelectionTagValueKey = Keys.combine(fullSelectionTagKey, tag_value);
         statementStore.putDescriptor(fullSelectionTagValueKey, key);
         
@@ -90,6 +97,33 @@ public final class TagGarden {
         statementStore.putDescriptor(regularTagTypeKey, tag_name);
     }
     
+    public Map<String, Map<String, String>> dissociateSelectionTag(String tag_name,
+                                                                   String key) {
+        requireOwnedSelectionTag(tag_name);
+        Keys.requiresValidKey(key);
+        
+        String fullSelectionTagKey = Keys.combine(selectionTagTypeKey, tag_name);
+        String selectionTagValue = getSelectionTagValue(tag_name, key);
+        if (selectionTagValue != null) {
+            String fullSelectionTagValueKey =
+                    Keys.combine(fullSelectionTagKey, selectionTagValue);
+            
+            String association = Keys.combine(fullSelectionTagValueKey, key);
+            
+            // Remove all associations with the association
+            Map<String, Map<String, String>> removed = entitySystem.remove(association);
+            
+            // Remove the association itself
+            String value = statementStore.removeWithDescriptor(fullSelectionTagValueKey, key);
+            removed.computeIfAbsent(fullSelectionTagValueKey, k -> new HashMap<>())
+                   .put(key, value);
+            
+            return removed;
+        }
+        
+        return new HashMap<>();
+    }
+    
     public Map<String, Map<String, String>> dissociateTag(String tag_name, String key) {
         requireOwnedRegularTag(tag_name);
         Keys.requiresValidKey(key);
@@ -112,8 +146,51 @@ public final class TagGarden {
         return new HashMap<>();
     }
     
+    public Set<String> getSelectionTags() {
+        return statementStore.getWithQualifier(selectionTagTypeKey).keySet();
+    }
+    
+    public String getSelectionTagValue(String tag_name, String key) {
+        Keys.requiresValidKey(key);
+        
+        String fullSelectionTagKey = Keys.combine(selectionTagTypeKey, tag_name);
+        Map<String, String> qualities = statementStore.getWithHolder(key);
+        for (Map.Entry<String, String> quality : qualities.entrySet()) {
+            String qualifier = quality.getKey();
+            
+            if (qualifier.contains(fullSelectionTagKey)) {
+                return Keys.getTip(qualifier);
+            }
+        }
+        
+        return null;
+    }
+    
+    public Set<String> getSelectionTagValues(String tag_name) {
+        SelectionTags.requireValidSelectionTag(tag_name);
+        
+        String fullSelectionTagKey = Keys.combine(selectionTagTypeKey, tag_name);
+        return statementStore.getWithQualifier(fullSelectionTagKey).keySet();
+    }
+    
     public Set<String> getTags() {
         return statementStore.getWithQualifier(regularTagTypeKey).keySet();
+    }
+    
+    public Map<String, Map<String, String>> removeSelectionTag(String tag_name) {
+        requireOwnedSelectionTag(tag_name);
+        
+        String fullSelectionTagKey = Keys.combine(selectionTagTypeKey, tag_name);
+        
+        // Remove any associations with the selection tag.
+        Map<String, Map<String, String>> removed = entitySystem.remove(fullSelectionTagKey);
+        
+        // Remove the selection tag itself
+        String value = statementStore.removeWithDescriptor(selectionTagTypeKey, tag_name);
+        removed.computeIfAbsent(selectionTagTypeKey, k -> new HashMap<>())
+               .put(tag_name, value);
+        
+        return removed;
     }
     
     public Map<String, Map<String, String>> removeTag(String tag_name) {
