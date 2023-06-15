eb541714ba76a9dc164ab0eb3ac1b77e3025ad2b
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-06-15 19:22:33 -0400

    Finished the tutorial

--- Gardener/src/program/Controller.java
+++ Gardener/src/program/Controller.java
@@ -2,19 +2,55 @@ package program;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class Controller {
+    private boolean start = true;
+    
     @FXML
     private Text output;
     
+    private long operand1; 
+    
+    private String operator = "";
+    
+    private Model model = new Model();
+    
     @FXML
     private void processNumpad(ActionEvent event) {
+        if (start) {
+            output.setText("");
+            start = false;
+        }
         
+        String value = ((Button) event.getSource()).getText();
+        
+        output.setText(output.getText() + value);
     }
     
     @FXML
     private void processOperator(ActionEvent event) {
+        String value = ((Button) event.getSource()).getText();
         
+        if (!value.equals("=")) {
+            if (!operator.isEmpty()) {
+                return;
+            }
+            operator = value;
+            operand1 = Long.parseLong(output.getText());
+            output.setText("");
+            
+        } else {
+            if (operator.isEmpty()) {
+                return;
+            }
+            
+            long result = model.calculate(operand1, Long.parseLong(output.getText()), operator);
+            output.setText(String.valueOf(result));
+            
+            operator = "";
+            start = true;
+        }
     }
 }
--- Gardener/src/program/Model.java
+++ Gardener/src/program/Model.java
@@ -1,5 +1,20 @@
 package program;
 
 public class Model {
-
+    public long calculate(long operand1,
+                          long operand2,
+                          String operator) {
+        switch (operator) {
+        case "+":
+            return operand1 + operand2;
+        case "-":
+            return operand1 - operand2;
+        case "*":
+            return operand1 * operand2;
+        case "/":
+            return operand1 / operand2;
+        }
+        
+        throw new IllegalArgumentException("Unknown operator");
+    }
 }
