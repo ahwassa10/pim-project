1dead4f7c4a3e8b192e522ae9214ad7387ceae0a
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2023-09-27 20:56:17 -0400

    Updated Tree.java

--- 2ver/src/model/trees/Tree.java
+++ 2ver/src/model/trees/Tree.java
@@ -20,54 +20,6 @@ public interface Tree<T> {
     
     boolean isChildNode(T node);
     
-    default T requireRootNode(T node) {
-        if (!contains(node) || !isRootNode(node)) {
-            String msg = String.format("%s is not the root node of this tree", node);
-            throw new IllegalArgumentException(msg);
-        }
-        return node;
-    }
-    
-    default T requireInteriorNode(T node) {
-        if (!contains(node) || !isInteriorNode(node)) {
-            String msg = String.format("%s is not an interior node in this tree", node);
-            throw new IllegalArgumentException(msg);
-        }
-        return node;
-    }
-    
-    default T requireLeafNode(T node) {
-        if (!contains(node) || !isLeafNode(node)) {
-            String msg = String.format("%s is not a leaf node in this tree", node);
-            throw new IllegalArgumentException(msg);
-        }
-        return node;
-    }
-    
-    default T requireParentNode(T node) {
-        if (!contains(node) || !isParentNode(node)) {
-            String msg = String.format("%s is not a parent node in this tree", node);
-            throw new IllegalArgumentException(msg);
-        }
-        return node;
-    }
-    
-    default T requireExteriorNode(T node) {
-        if (!contains(node) || !isExteriorNode(node)) {
-            String msg = String.format("%s is not an exterior node in this tree", node);
-            throw new IllegalArgumentException(msg);
-        }
-        return node;
-    }
-    
-    default T requireChildNode(T node) {
-        if (!contains(node) || !isChildNode(node)) {
-            String msg = String.format("%s is not a child node in this tree", node);
-            throw new IllegalArgumentException(msg);
-        }
-        return node;
-    }
-    
     T getRoot();
     
     T getParent(T node);
